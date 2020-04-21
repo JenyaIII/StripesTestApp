d8cd8883383807871ba27fe5b310596f86e3e565
JenyaIII <161russ@gmail.com>
2020-04-21 15:10:54 +0300

    Price and checkout object added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+	const stripe = useStripe();
+
+	return (
+		<form className="checkoutForm">
+			<img
+				className="checkoutObject"
+				src="https://www.apple.com/v/mac/home/ao/images/overview/hero/macbook_air__csdfieli984m_large.jpg"
+				alt="magazine"
+			/>
+			<h2>Price: $780 USD</h2>
+			<CardElement />
+			<button type="submit" disabled={!stripe} className="payThis">
+				Pay
+			</button>
+		</form>
+	);
+};
+
+const stripePromise = loadStripe('pk_test_4RNbN71ILTJjNN6KCTndGLDG00CmKwjOcX');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Stripe Billing System Page</h1>
+			<Elements stripe={stripePromise}>
+				<CheckoutForm />
+			</Elements>
+		</div>
+	);
 }
 
 export default App;
